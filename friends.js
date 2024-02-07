8360a8efaeb1cdce2361749e332041abb4282309
Ofirw12 <ofir12121@gmail.com>
2024-02-07 16:33:32 +0200

    CSS styles connect

--- friends.js
+++ friends.js
@@ -1,3 +1,14 @@
 const selectElement = document.querySelector('.confirm-deny');
 
 selectElement.addEventListener('change', (e) => this.form.submit());
+
+const friendsList = document.querySelector('my-friends-list')
+const countOfFriends = document.getElementById('total')
+const rows = document.querySelectorAll('.friendRow')
+const count = () =>{
+    let total = 0;
+    rows.forEach((row) => total += 1);
+    return total;
+}
+
+countOfFriends.textContent = count();
